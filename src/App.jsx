7818d1553098b1bb42a65fa1a47d0d2bd4790f13
elliotgaramendi <elliotgaramendi@gmail.com>
2025-06-11 17:02:23 -0500

    feat: initialize React project with Vite, Bootstrap, and ESLint

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section>
+          <div className="container">
+            <h2>Las mejores películas de la historia</h2>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
